5a54dbbb95eb8630fe93b418c22ae8f150927aba
kida0214 <kidawenzi@gmail.com>
2025-06-25 22:50:11 +0900

    選択科目の表示

--- resources/views/authenticated/users/search.blade.php
+++ resources/views/authenticated/users/search.blade.php
@@ -1,61 +1,71 @@
 <x-sidebar>
-<p>ユーザー検索</p>
-<div class="search_content w-100 border d-flex">
-  <div class="reserve_users_area">
-    @foreach($users as $user)
-    <div class="border one_person">
-      <div>
-        <span>ID : </span><span>{{ $user->id }}</span>
-      </div>
-      <div><span>名前 : </span>
-        <a href="{{ route('user.profile', ['id' => $user->id]) }}">
-          <span>{{ $user->over_name }}</span>
-          <span>{{ $user->under_name }}</span>
-        </a>
-      </div>
-      <div>
-        <span>カナ : </span>
-        <span>({{ $user->over_name_kana }}</span>
-        <span>{{ $user->under_name_kana }})</span>
-      </div>
-      <div>
-        @if($user->sex == 1)
-        <span>性別 : </span><span>男</span>
-        @elseif($user->sex == 2)
-        <span>性別 : </span><span>女</span>
-        @else
-        <span>性別 : </span><span>その他</span>
-        @endif
-      </div>
-      <div>
-        <span>生年月日 : </span><span>{{ $user->birth_day }}</span>
-      </div>
-      <div>
-        @if($user->role == 1)
-        <span>権限 : </span><span>教師(国語)</span>
-        @elseif($user->role == 2)
-        <span>権限 : </span><span>教師(数学)</span>
-        @elseif($user->role == 3)
-        <span>権限 : </span><span>講師(英語)</span>
-        @else
-        <span>権限 : </span><span>生徒</span>
-        @endif
-      </div>
-      <div>
+  <p>ユーザー検索</p>
+  <div class="search_content w-100 border d-flex">
+    <div class="reserve_users_area">
+      @foreach($users as $user)
+      <div class="border one_person">
+        <div>
+          <span>ID : </span><span>{{ $user->id }}</span>
+        </div>
+        <div>
+          <span>名前 : </span>
+          <a href="{{ route('user.profile', ['id' => $user->id]) }}">
+            <span>{{ $user->over_name }}</span>
+            <span>{{ $user->under_name }}</span>
+          </a>
+        </div>
+        <div>
+          <span>カナ : </span>
+          <span>({{ $user->over_name_kana }}</span>
+          <span>{{ $user->under_name_kana }})</span>
+        </div>
+        <div>
+          <span>性別 : </span>
+          @if($user->sex == 1)
+            <span>男</span>
+          @elseif($user->sex == 2)
+            <span>女</span>
+          @else
+            <span>その他</span>
+          @endif
+        </div>
+        <div>
+          <span>生年月日 : </span><span>{{ $user->birth_day }}</span>
+        </div>
+        <div>
+          <span>権限 : </span>
+          @if($user->role == 1)
+            <span>教師(国語)</span>
+          @elseif($user->role == 2)
+            <span>教師(数学)</span>
+          @elseif($user->role == 3)
+            <span>講師(英語)</span>
+          @else
+            <span>生徒</span>
+          @endif
+        </div>
         @if($user->role == 4)
-        <span>選択科目 :</span>
+        <div>
+          <span>選択科目 : </span>
+          @if($user->subjects->isNotEmpty())
+            @foreach($user->subjects as $subject)
+              <span>{{ $subject->subject }}@if(!$loop->last)、@endif</span>
+            @endforeach
+          @else
+            <span>未登録</span>
+          @endif
+        </div>
         @endif
       </div>
+      @endforeach
     </div>
-    @endforeach
-  </div>
-  <div class="search_area w-25 border">
-    <div class="">
+
+    <div class="search_area w-25 border">
       <div>
         <input type="text" class="free_word" name="keyword" placeholder="キーワードを検索" form="userSearchRequest">
       </div>
       <div>
-        <lavel>カテゴリ</lavel>
+        <label>カテゴリ</label>
         <select form="userSearchRequest" name="category">
           <option value="name">名前</option>
           <option value="id">社員ID</option>
@@ -68,7 +78,7 @@
           <option value="DESC">降順</option>
         </select>
       </div>
-      <div class="">
+      <div>
         <p class="m-0 search_conditions"><span>検索条件の追加</span></p>
         <div class="search_conditions_inner">
           <div>
@@ -84,7 +94,7 @@
               <option value="1">教師(国語)</option>
               <option value="2">教師(数学)</option>
               <option value="3">教師(英語)</option>
-              <option value="4" class="">生徒</option>
+              <option value="4">生徒</option>
             </select>
           </div>
           <div class="selected_engineer">
@@ -98,8 +108,7 @@
       <div>
         <input type="submit" name="search_btn" value="検索" form="userSearchRequest">
       </div>
+      <form action="{{ route('user.show') }}" method="get" id="userSearchRequest"></form>
     </div>
-    <form action="{{ route('user.show') }}" method="get" id="userSearchRequest"></form>
   </div>
-</div>
 </x-sidebar>
